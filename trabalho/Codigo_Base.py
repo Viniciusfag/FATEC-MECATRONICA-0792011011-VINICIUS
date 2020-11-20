2938583a5f1e1af4db232c6e6831f16bbc81f923
Fagundes2341 <62109322+Fagundes2341@users.noreply.github.com>
2020-11-20 09:52:03 -0300

    Create Codigo_Base.py

---
+++ trabalho/Codigo_Base.py
@@ -0,0 +1,120 @@
+def classificacao():
+  if idade >= 18:
+    print(genero["18"], genero["16"], genero["14"], genero["12"], genero["10"], genero["Livre"])
+  elif idade >= 16:
+    print(genero["16"], genero["14"], genero["12"], genero["10"], genero["Livre"])
+  elif idade >= 14:
+    print(genero["14"], genero["12"], genero["10"], genero["Livre"])
+  elif idade >= 12:
+    print(genero["12"], genero["10"], genero["Livre"])
+  elif idade >= 10:
+    print(genero["10"], genero["Livre"])
+  elif idade < 10:
+    print(genero["Livre"])
+
+Drama = {}
+
+Drama["18"] = "13 - O Jogador"
+Drama["16"] = "127 Horas"
+Drama["14"] = "1917"
+Drama["12"] = "Creed: Nascido Para Lutar"
+Drama["10"] = "As Aventuras de PI"
+Drama["Livre"] = "A Felicidade Não Se Compra"
+
+Comedia = {}
+
+Comedia["18"] = "Kick-Ass: Quebrando Tudo"
+Comedia["16"] = "Rick e Morty"
+Comedia["14"] = "A Noite do Jogo"
+Comedia["12"] = "Gente Grande"
+Comedia["10"] = "O Retorno de Johnny English"
+Comedia["Livre"] = "O Mentiroso"
+
+Terror = {}
+
+Terror["18"] = "Esta é a sua morte"
+Terror["16"] = "A Hora do Pesadelo"
+Terror["14"] = "O Silêncio dos Inocentes"
+Terror["12"] = "Poltergeist"
+Terror["10"] = "ParaNorman"
+Terror["Livre"] = "A Casa Monstro"
+
+Açao = {}
+
+Açao["18"] = "Rambo: Até o Fim"
+Açao["16"] = "Lucy"
+Açao["14"] = "Venom"
+Açao["12"] = "Duro de Matar: Um Bom Dia Para Morrer"
+Açao["10"] = "Bumblebee"
+Açao["Livre"] = "Pequenos Espiões"
+
+Animaçao = {}
+
+Animaçao["18"] = "para 18"
+Animaçao["16"] = "para 16"
+Animaçao["14"] = "para 14"
+Animaçao["12"] = "para 12"
+Animaçao["10"] = "para 10"
+Animaçao["Livre"] = "Livre"
+
+Ficçao_Cientifica = {}
+
+Ficçao_Cientifica["18"] = "para 18"
+Ficçao_Cientifica["16"] = "para 16"
+Ficçao_Cientifica["14"] = "para 14"
+Ficçao_Cientifica["12"] = "para 12"
+Ficçao_Cientifica["10"] = "para 10"
+Ficçao_Cientifica["Livre"] = "Livre"
+
+Romance = {}
+
+Romance["18"] = "Jogo Perigoso"
+Romance["16"] = "Cinquenta Tons de Cinza"
+Romance["14"] = "Titanic"
+Romance["12"] = "Nerve - Um jogo Sem Regras"
+Romance["10"] = "O Amor Não Tira Férias"
+Romance["Livre"] = "Legalmente Loira"
+
+Suspense = {}
+
+Suspense["18"] = "Scarface"
+Suspense["16"] = "Parasita"
+Suspense["14"] = "Tubarão"
+Suspense["12"] = "Truque de Mestre"
+Suspense["10"] = "O Mistério das Palavras Cruzadas"
+Suspense["Livre"] = "para todos"
+
+idade = int(input("qual a sua idade? "))
+tipo = input("insira o tipo ")
+
+if tipo == "Drama":
+  genero = Drama
+  classificacao()
+
+if tipo == "Comedia":
+  genero = Comedia
+  classificacao()
+
+if tipo == "Terror":
+  genero = Terror
+  classificacao()
+
+if tipo == "Açao":
+  genero = Açao
+  classificacao()
+
+if tipo == "Animaçao":
+  genero = Animaçao
+  classificacao()
+
+if tipo == "Ficçao Cientifica":
+  genero = Ficçao_Cientifica
+  classificacao()
+
+if tipo == "Romance":
+  genero = Romance
+  classificacao()
+
+if tipo == "Suspense":
+  genero = Suspense
+  classificacao()
